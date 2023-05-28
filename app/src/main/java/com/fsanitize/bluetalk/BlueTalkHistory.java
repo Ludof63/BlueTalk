783975c9b0f12eaf811dad8c1b46f17479c60a0f
ludof63 <f63capia@gmail.com>
2023-05-28 19:52:52 +0200

    Added chat history support first version, added icon

---
+++ app/src/main/java/com/fsanitize/bluetalk/BlueTalkHistory.java
@@ -0,0 +1,78 @@
+package com.fsanitize.bluetalk;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BlueTalkHistory {
+    private static final String LOG_TAG = "BlueTalkHistory";
+    static HashMap<String, List<BluetoothMessage>> chatHistory = new HashMap<>();
+    private static final String CHAT_HISTORY = "chat_history";
+    private static final String HISTORY_USERS = "history_users";
+
+    public static <T> List<T> stringToArray(String s, Class<T[]> clazz) {
+        T[] arr = new Gson().fromJson(s, clazz);
+        return Arrays.asList(arr);
+    }
+
+    public void storeHistory(Context context){
+        Log.d(LOG_TAG, "ChatHistory:  storing chat histories");
+        Gson gson = new Gson();
+        SharedPreferences pref = context.getSharedPreferences(HISTORY_USERS,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        String json;
+
+        //saving users with a chat history
+        String[] users = chatHistory.keySet().toArray(new String[0]);
+        json = gson.toJson(users);
+        editor.putString(HISTORY_USERS, json);
+        editor.commit();
+
+        //saving every user chat history
+        for(String user : chatHistory.keySet()){
+            List<BluetoothMessage> user_history = chatHistory.get(user);
+            json = gson.toJson(user_history);
+            editor.putString(user,json);
+            editor.commit();
+        }
+    }
+    public void restoreHistory(Context context){
+        Log.d(LOG_TAG, "ChatHistory:  restoring chat histories");
+        Gson gson = new Gson();
+        SharedPreferences pref = context.getSharedPreferences(HISTORY_USERS,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        String json;
+        List<BluetoothMessage> history = new LinkedList<>();
+
+        //getting users with a chat history
+        json = pref.getString(HISTORY_USERS,"");
+        String[] users = gson.fromJson(json,String[].class);
+        if(users == null)
+            return;
+
+        //retrieving every user history
+        for(String user : users){
+            json = pref.getString(user,"");
+            history = stringToArray(json, BluetoothMessage[].class);
+            chatHistory.put(user,history);
+        }
+    }
+    public void updateUserHistory(String userAddress, List<BluetoothMessage> newChatHistory){
+        chatHistory.put(userAddress,newChatHistory);
+    }
+
+    public List<BluetoothMessage> getUserHistory(String userAddress){
+        if(chatHistory.containsKey(userAddress))
+            return chatHistory.get(userAddress);
+        return null;
+    }
+
+}
